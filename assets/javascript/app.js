8be74675da50581bb209bb59c93502d562b00e84
Michael N <mnos9897@gmail.com>
2019-08-10 12:27:35 -0400

    formatting js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,15 +3,53 @@ let spotifyURL = "https://accounts.spotify.com/authorize?";
 // grabs the zipcode from user
 // let $search = $("#").val();
 
-$(document).ready(function() {
+$(document).ready(function () {
+
+    //weather variables
+    let userZip,
+        weatherData,
+        cityName,
+        temp,
+        weather
+
+    let getWeather = function () {
+
+        // queryURL = `api.openweathermap.org/data/2.5/weather?zip=${userZip}&APPID=01b094dd158ecf4fb77c7c5db98a6ad6`
+        let queryURL = `https://api.openweathermap.org/data/2.5/weather?zip=03053&APPID=01b094dd158ecf4fb77c7c5db98a6ad6`
+
+        $.get(queryURL).then(function (response) {
+            weatherData = response;
+
+            console.log(weatherData);
+
+            //display location
+            cityName = weatherData.name;
+            let cityP = $("<p>").text(cityName);
+            $("div#city").append(cityP);
+
+            // display weather
+            weather = weatherData.weather[0].description;
+            let weatherP = $("<p>").text(weather);
+            $("div#weather-icon").append(weatherP);
+
+            // display temperature
+            temp = (parseInt(weatherData.main.temp) - 273.15) * 9 / 5 + 32;
+            temp = Math.round(100 * temp) / 100
+            let tempP = $("<p>").text(temp);
+            $("div#temperature").append(tempP)
+
+        });
+    }
+
+    getWeather();
 
     // builds query params
     let queryParam = {
-        client_id : "ff7637f2ebb2489d9a2c5a6dca7c208c",
-        response_type : "code",
-        redirect_uri : "https://mmnos.github.io/Project1/",
-        q : "rihanna",
-        limit : 3
+        client_id: "ff7637f2ebb2489d9a2c5a6dca7c208c",
+        response_type: "code",
+        redirect_uri: "https://mmnos.github.io/Project1/",
+        q: "rihanna",
+        limit: 3
     }
 
     // converts the query param object to a string for the url
@@ -24,7 +62,7 @@ $(document).ready(function() {
 
     // makes an AJAX request to get data from spotify
     $.get({
-        url : completeSpotifyURL
+        url: completeSpotifyURL
     }).then(updatePage);
 
     // updates the page with new playlists 
@@ -39,43 +77,3 @@ $(document).ready(function() {
     }
 
 });
-
-//weather variables
-let userZip,
-    weatherData,
-    cityName,
-    temp,
-    weather
-
-
-
-let getWeather = function() {
-
-  // queryURL = `api.openweathermap.org/data/2.5/weather?zip=${userZip}&APPID=01b094dd158ecf4fb77c7c5db98a6ad6`
-  let queryURL = `https://api.openweathermap.org/data/2.5/weather?zip=03053&APPID=01b094dd158ecf4fb77c7c5db98a6ad6`
-  
-  $.get(queryURL).then(function(response){
-    weatherData = response;
-
-    console.log(weatherData);
-
-    //display location
-    cityName = weatherData.name;
-    let cityP = $("<p>").text(cityName);
-    $("div#city").append(cityP);
-
-    // display weather
-    weather = weatherData.weather[0].description;
-    let weatherP = $("<p>").text(weather);
-    $("div#weather-icon").append(weatherP);
-
-    // display temperature
-    temp = (parseInt(weatherData.main.temp) - 273.15) * 9 / 5 + 32;
-    temp = Math.round(100 * temp)/100
-    let tempP = $("<p>").text(temp);
-    $("div#temperature").append(tempP)
-
-  });
-}
-
-getWeather();

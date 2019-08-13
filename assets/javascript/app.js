fcd8fc4146c4c0dca3f59613c48b7c4ef6d9edcd
Michael N <mnos9897@gmail.com>
2019-08-13 15:20:55 -0400

    updated html/css, added change location

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,13 +1,16 @@
 $(document).ready(function () {
 
-  // weather variables
-  let userZip,
-    weatherData,
-    cityName,
-    temp,
-    weather
+    // weather variables
+    let userZip,
+        weatherData,
+        cityName,
+        temp,
+        weather
 
-  let getWeather = function () {
+    $("#weather-data").hide();
+    $(".helper-text").hide();
+
+    let getWeather = function () {
 
         // queryURL = `api.openweathermap.org/data/2.5/weather?zip=${userZip}&APPID=01b094dd158ecf4fb77c7c5db98a6ad6`
         let queryURL = `https://api.openweathermap.org/data/2.5/weather?zip=${userZip}&APPID=01b094dd158ecf4fb77c7c5db98a6ad6`
@@ -16,29 +19,32 @@ $(document).ready(function () {
 
             weatherData = response;
 
-      console.log(weatherData);
+            console.log(weatherData);
 
-      //display location
-      cityName = weatherData.name;
-      let cityP = $("<p>").text(cityName);
-      $("div#city").append(cityP);
+            //display location
+            cityName = weatherData.name;
+            let cityP = $("<span>").text(cityName).addClass("updatedWeather");
 
-      // display weather
-      weather = weatherData.weather[0].description;
-      let weatherP = $("<p>").text(weather);
-      $("div#weather-icon").append(weatherP);
+            // display weather
+            weather = weatherData.weather[0].description;
+            let weatherP = $("<span>").text(weather + " in ").addClass("updatedWeather");
+            $("div#weather-text").append(weatherP, cityP);
 
-      // display temperature
-      temp = (parseInt(weatherData.main.temp) - 273.15) * 9 / 5 + 32;
-      temp = Math.round(100 * temp) / 100
-      let tempP = $("<p>").text(temp);
-      $("div#temperature").append(tempP)
+            // display temperature
+            temp = (parseInt(weatherData.main.temp) - 273.15) * 9 / 5 + 32;
+            temp = Math.round(temp);
+            let tempP = $("<span>").text(temp + "°").addClass("updatedWeather").css("font-size", "50px");
+            $("div#temperature").prepend(tempP);
 
-    });
+        });
 
-  };
+        $("#weather-data").show();
 
-    $("#submitZip").on("click", function(event) {
+    };
+
+    let checkZip = function () {
+
+        $(".updatedWeather").empty();
 
         // holds user input
         userZip = $("#zipcode").val().trim();
@@ -53,54 +59,82 @@ $(document).ready(function () {
         if (regex.test(userZip)) {
 
             getWeather();
+            $(".helper-text").hide();
+            $(".input-field").hide();
 
         } else {
 
-            console.log("Please enter a valid zipcode");
+            $(".helper-text").show();
+
+        }
+
+        // clears input field after clicking submit
+        $("#zipcode").val('');
+
+    }
+
+    $("#submitZip").on("click", function (event) {
+
+        checkZip();
+
+    });
+
+    $("#changeZip").on("click", function (event) {
+
+        $(".input-field").show();
+        $("#weather-data").hide();
+
+    });
+
+    $(document).on("keypress", function (ent) {
+
+        if (ent.which === 13) {
+
+            checkZip();
 
         }
 
     });
 
     // var to hold access token
-    let accessToken = "BQDus3jIgKsGevd_DUzETFOF_dqM_jN7_6-OMQRjg6U3rardIRBX-tLkverWChstGBbTKO3ZhLDfHQlny-qSZap3HnNELY6KCtTuTg8KmSzMgD4E7VmYUKsNDw4SzLZmTtfEQPsgyxIm332EzRtIzZ11FOZWHdm0c_ddH9pNudV-ieGpUUChg1QlBQaYm4HLVd7Avv6TCpca6Gk";
+    let accessToken = "BQAn1gI37gBIHteXExbd1AuLXUPveg1va-_u6Ddw4Gwf-t3fzwx3P52j9xOKKfVh917uzFZB9FSWEhxZ2PqT2xsVTsmfy2AZAI4my2fmL46xJA-dSf-dZXMHx2WJpQ1hxZUDLT9JbG9xXoWMf1Ik38OHN-JoC8DaOY6mNehAkKBsxqJXPk5W8pStH98duQnl1B6X9EaQkS3n";
 
-  // makes an ajax request to search the spotify api with recommended playlists
-  $.get({
+    // makes an ajax request to search the spotify api with recommended playlists
+    $.get({
 
-    url: 'https://api.spotify.com/v1/search?q=rainy&type=playlist&limit=5',
-    headers: {
-      'Authorization': 'Bearer ' + accessToken
-    },
-    success: function (response) {
+        url: 'https://api.spotify.com/v1/search?q=rainy&type=playlist&limit=5',
+        headers: {
+            'Authorization': 'Bearer ' + accessToken
+        },
+        success: function (response) {
 
-      console.log(response);
-      let playlists = response.playlists.items
+            console.log(response);
+            let playlists = response.playlists.items
 
-      console.log(playlists)
+            console.log(playlists);
 
 
-      for (i = 0; i < playlists.length; i++) {
+            for (i = 0; i < playlists.length; i++) {
 
-        let playlistName = playlists[i].name;
-        let imgSrc = playlists[i].images[0].url
-        let redirect = playlists[i].external_urls.spotify
+                let playlistName = playlists[i].name;
+                let imgSrc = playlists[i].images[0].url;
+                let redirect = playlists[i].external_urls.spotify;
 
-        let $name = $("<h4>").text(playlistName);
+                let $name = $("<h4>").text(playlistName);
 
-        let $img = $("<img>")
-          .attr("src", imgSrc)
-          .css("cursor", "pointer")
+                let $img = $("<img>")
+                    .attr("src", imgSrc)
+                    .css("cursor", "pointer");
 
-        let $a = $("<a>")
-          .attr("href", `${redirect}`)
-          .append($img)
+                let $a = $("<a>")
+                    .attr("href", `${redirect}`)
+                    .append($img);
 
-        $(`div#${i}`).append($name, $a);
+                $(`div#${i}`).append($name, $a);
 
-      }
-    }
+            }
+        }
 
-  });
+    });
 
 });
